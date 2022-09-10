c054a008051294a0a480bc4473650ed25f423e0d
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2022-09-10 14:26:34 +0300

    add the append code

--- __test__/linkedlist.test.js
+++ __test__/linkedlist.test.js
@@ -11,6 +11,7 @@ describe('Testing LinkedList', () => {
     });
 });
 
+
 describe('Inserting into the linked list (beginning)', () => {
     it('insert into empty linked list', () => {
         const list = new LinkedList();
@@ -26,6 +27,7 @@ describe('Inserting into the linked list (beginning)', () => {
         list.insert(value);                          // insert a value into the list
         expect(list.head.value).toEqual(value);      // check if the value is equal to the value inserted
         expect(list.head.next).toBeNull();           // check if the next value is null
+        
         const value2 = 10;
         list.insert(value2);                         // insert a value into the list
         expect(list.head.value).toEqual(value2);     // check if the value is equal to the value inserted
@@ -33,3 +35,28 @@ describe('Inserting into the linked list (beginning)', () => {
         expect(list.head.next.next).toBeNull();      // check if the next value is null
     });
 });
+
+
+describe('append to the end of the linked list', () => {
+    it('append to empty linked list', () => {
+        const list = new LinkedList();
+        const value = 5;
+        list.append(value);                          // append a value into the list
+        expect(list.head.value).toEqual(value);      // check if the value is equal to the value inserted
+        expect(list.head.next).toBeNull();           // check if the next value is null
+    });
+
+    it('append to not empty linked list ', () => {
+        const list = new LinkedList();
+        const value = 5;
+        list.append(value);                          // append a value into the list
+        expect(list.head.value).toEqual(value);      // check if the value is equal to the value inserted
+        expect(list.head.next).toBeNull();           // check if the next value is null
+        
+        const value2 = 10;
+        list.append(value2);                         // append a value into the list
+        expect(list.head.value).toEqual(value);      // check if the value is equal to the value inserted
+        expect(list.head.next.value).toEqual(value2);// check if the next value is equal to the value inserted
+        expect(list.head.next.next).toBeNull();      // check if the next value is null
+    });
+} );
--- linkedlist.js
+++ linkedlist.js
@@ -18,6 +18,19 @@ class LinkedList {
 
         // this.head = new Node(value, this.head);  //create a new node and assign it to the head
     }
+
+    append(value) {
+        const node = new Node(value);
+        if(!this.head) {           // if the linked list is empty
+          this.head = node;
+        } else {                   // if linked list is not empty
+          let current = this.head;
+          while(current.next) {    // while there is a next node
+            current = current.next;
+          }
+          current.next = node;     // assign the new node to the next of the last node
+        }
+    }
 }
 
 module.exports = LinkedList;
